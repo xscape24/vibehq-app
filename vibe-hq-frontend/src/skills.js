448d4eab97f68939f06d2c2d5d5021bcc7a8c027
Eben Nduwimana <ebenduwimana@gmail.com>
2026-09-05 13:58:53 -0400

    Add dynamic styling to task list and update design

---
+++ vibe-hq-frontend/src/skills.js
@@ -0,0 +1,22 @@
+function fr(){
+    console.log("Hello from skills.js");
+}
+
+   function popupFn() {
+            document.getElementById(
+                "overlay"
+            ).style.display = "block";
+            document.getElementById(
+                "popupDialog"
+            ).style.display = "block";
+        }
+
+
+   function closeFn() {
+            document.getElementById(
+                "overlay"
+            ).style.display = "none";
+            document.getElementById(
+                "popupDialog"
+            ).style.display = "none";
+        }
--- vibe-hq-frontend/src/tracker.js
+++ vibe-hq-frontend/src/tracker.js
@@ -23,35 +23,62 @@ function changeSettingsBox(int){
     const selectedBox=document.getElementById(settings-box-1);
     settingsSection.innerHTML=selectedBox.innerHTML;
 }
+
+
+
+
+
 function addItem(){ //add item to list
+    const span=document.createElement("span");
+    span.style.backgroundColor=" #384056";
+    span.style.display="flex";
+    span.style.justifyContent="center";
+    span.style.alignItems="center";
+    span.style.textAlign="center";
+    span.style.width="100%";
+    span.style.margin="auto";
+    span.style.paddingTop="1vh";
+
+
+    
+    span.style.color="orange";
     var li= document.createElement("li");
     li.className="todo-item";
-    li.style.fontSize="2vw";
+    li.style.fontSize="1.5vw";
+    li.style.border="none";
     var inputValue =document.getElementById("todo-input").value;
     
     if(inputValue===''){
         alert("No item entered!");
     }else{
         var text=document.createTextNode(inputValue);
+        
+       
         li.appendChild(text);
+       
         
         var x =document.createElement("button");
+        x.style.backgroundColor="#70768d";
         x.style.color="red";
-        x.style.height="3vw";
+        x.style.height="80%";
         x.style.width="4vw";
-        x.style.fontSize="1vw";
+        x.style.fontSize="2vw";
         x.innerText="X";
+        x.style.fontWeight="500";
+        x.style.float="right";
 
-        x.style.borderRadius="1vw";
-        x.style.marginLeft="";
+        x.style.borderRadius=".3vw";
+        x.style.marginLeft="3vw";
         x.className="remove-btn";
 
         x.id=inputValue;
         li.appendChild(x)
-        var todolist=document.getElementById("to-do-list");
-        todolist.appendChild(li);
+        span.appendChild(li);
+        var todolist=document.getElementById("tasks-list");
+        
+        todolist.appendChild(span);
         document.getElementById("todo-input").placeholder="";
-        document.getElementById("todo-input").value="Enter an item";
+        document.getElementById("todo-input").value="";
         
     }
 }
