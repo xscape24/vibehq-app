1453795d7c1d2adcafe6ad8ea436e957b5052da7
Eben Nduwimana <ebenduwimana@gmail.com>
2026-09-12 19:43:54 -0400

    Apply theme change dynamically and design update

--- vibe-hq-frontend/src/script.js
+++ vibe-hq-frontend/src/script.js
@@ -1,19 +1,32 @@
+//Variables
+
+const output=document.querySelector('output');
+
+
+
+function cGallery(){
+    const gallery=document.createElement('div');
+    output.append(gallery);
+}
+
+
+
+
+
+
+
+
+
+
 function changeScreenLeft(){
-const btn=document.getELementById("left-arrow");
 
-const soundImg=document.getELementById('sound-img');
+document.getElementById('sound-img').setAttribute("src","assets/study-girl.png");
 
 
-btn.addEventListener('click', function() {
-    // 3. Change the src attribute to the new image path
-    soundImg.src ='assets/land1.jpg';
-
-});
 }
 function changeScreenRight(){
-const btn=document.querySelector('.right-arrow');
-const screenContainer=document.querySelector('.screen-container');
-const soundImg=document.querySelector('.sound-img');
-const soundAudio=document.querySelector('.sound-audio');
+
+document.getElementById('sound-img').setAttribute("src","assets/study-girl.png");
+
 
 }
