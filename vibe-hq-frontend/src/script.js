b11019d4e38ae0cab123e7a72d2e8186347001e6
Eben Nduwimana <ebenduwimana@gmail.com>
2026-09-16 20:28:49 -0400

    Javascript update image in dashboard

--- vibe-hq-frontend/src/script.js
+++ vibe-hq-frontend/src/script.js
@@ -19,14 +19,37 @@ function cGallery(){
 
 
 function changeScreenLeft(){
+    const image=document.getElementById('sound-image');
 
-document.getElementById('sound-img').setAttribute("src","assets/study-girl.png");
+
+    var str=image.src;
+var ch=str.charAt(str.length-5);
+var num=parseInt(ch,10);
+num=num-1;
+if(num !=0){
+ch=String.fromCharCode(num);
+str=str.substring(0,str.length-5);
+str=str+ch+".jpg";
+
+
+    document.getElementById('sound-img').setAttribute("src",str);
+}
 
 
 }
 function changeScreenRight(){
 
-document.getElementById('sound-img').setAttribute("src","assets/study-girl.png");
+   const image=document.getElementById('sound-image');
+
+
+    var str=image.src;
+var ch=str.charAt(str.length-5);
+var num=parseInt(ch,10);
+num=num+1;
+ch=String.fromCharCode(num);
+str=str.substring(0,str.length-5);
+str=str+ch+".jpg";
+  document.getElementById('sound-img').setAttribute("src",str);
 
 
 }
